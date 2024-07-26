fcea41cf72fd5fcbe3fb7de88ab179b4a94b7463
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-07-26 15:35:58 +0200

    More dynamic tests

--- src/test/scala/io/vasilev/FileTestSuite.scala
+++ src/test/scala/io/vasilev/FileTestSuite.scala
@@ -1,10 +1,13 @@
+package io.vasilev
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class FileTestSuite extends AnyFunSuite with Matchers {
-  Seq(1, 2, 3).foreach { x =>
+  (1 to 10).foreach { x =>
     test(s"test $x") {
-      None shouldBe empty
+      if (x % 2 == 0) succeed
+      else 1 shouldBe 2
     }
   }
 }
